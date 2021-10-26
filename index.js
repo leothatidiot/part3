559d360f3db66dc352a31df8b39f407ae32dbb90
Leo <cenliwen03@gmail.com>
2021-10-26 15:39:03 +0800

    part3 course

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const cors = require('cors')
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false 
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true
+  }
+]
+
+// const http = require('http')
+// const app = http.createServer( (request, response) => {
+//   response.writeHead(200, {'Content-Type': 'text/plain'})
+//   response.end(JSON.stringify(notes))
+// })
+
+// 改用 express
+const app = express()
+
+app.use(express.json()) // json-parser
+app.use(cors()) // 跨来源(域)资源共享
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:', request.path)
+  console.log('Body:', request.body)
+  console.log('---')
+  next()
+}
+app.use(requestLogger) // HTTP request 日志
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+  const note = request.body
+  console.log(note)
+  response.json(note)
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error:'unknown endpoint'})
+}
+app.use(unknownEndpoint)
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
+
+// npm run dev
