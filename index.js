08420aebe91ed8e31130aa4d7121cea554ceafd8
Leo <cenliwen03@gmail.com>
2021-11-23 22:15:16 +0800

    part3/c

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const Note = require('./models/note')
 
 let notes = [
   {
@@ -30,7 +32,7 @@ let notes = [
 
 // 改用 express
 const app = express()
-
+app.use(express.static('build')) // 后端部署静态文件
 app.use(express.json()) // json-parser
 app.use(cors()) // 跨来源(域)资源共享
 
@@ -41,38 +43,76 @@ const requestLogger = (request, response, next) => {
   console.log('---')
   next()
 }
-app.use(requestLogger) // HTTP request 日志
+app.use(requestLogger) // HTTP request 日志 中间件
 
-app.use(express.static('build')) // 后端部署静态文件
-
+// route handler
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  // response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/notes/:id', (request, response, next) => {
+  // const id = Number(request.params.id)
+  // const note = notes.find(note => note.id === id)
+  // if (note) {
+  //   response.json(note)
+  // } else {
+  //   response.status(404).end()
+  // }
+  Note.findById(request.params.id).then(note => {
+    if (note) {
+      response.json(note)
+    } else { // 没找到的话会报 CastError (promise reject) 直接进入 catch 而不会进入 else
+      response.status(404).end()
+    }
+  }).catch(error => {
+    // console.log(error)
+    // response.status(400).send({error: 'malformatted id'})
+    next(error)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-  response.status(204).end()
+  // const id = Number(request.params.id)
+  // notes = notes.filter(note => note.id !== id)
+  // response.status(204).end()
+  Note.findByIdAndRemove(request.params.id).then(result => {
+    response.status(204).end()
+  }).catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
-  const note = request.body
-  console.log(note)
-  response.json(note)
+  const body = request.body
+  // console(note)
+  // response.json(note)
+  if(body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date()
+  })
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+  Note.findByIdAndUpdate(request.params.id, note, {new: true}).then(updateNote => {
+    response.json(updateNote)
+  }).catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -80,8 +120,17 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({error: 'malformatted id'})
+  }
+  next(error)
+}
+app.use(errorHandler) // 将错误处理移入中间件
+
+const PORT = process.env.PORT
 app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+console.log(`Server running on port ${PORT}`) 
 
 // npm run dev
---
+++ models/note.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+// connect MongoDB
+const url = process.env.MONGODB_URI
+console.log('connecting to ', url)
+
+mongoose.connect(url).then(result => {
+  console.log('connected to MongoDB')
+}).catch( error => {
+  console.log('error connecting to MongoDB:', error.message)
+})
+
+// define schema
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+// modify schema toJson method
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
